885db3f7df81790d113d4d3db89b7ae421eee040
Safi <safisiddiquim@gmail.com>
2022-07-12 15:41:33 -0500

    added search feature

---
+++ src/Components/pages/Exercises.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
+import ExerciseCard from "./ExerciseCard";
+import { exerciseOptions, fetchData } from "../../utils/fetchData";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
+  //Paginate
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exerciseData = [];
+      if (bodyPart === "all") {
+        exerciseData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exerciseData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+        setExercises(exerciseData);
+      }
+    };
+    fetchExercisesData();
+  }, [bodyPart, setExercises]);
+
+  return (
+    <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="46px"
+      >
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            sixe="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Exercises;
